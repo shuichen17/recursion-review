96c2b19515fc316ecde3f84c0cf71e4514fd9d00
shuichen17 <shuichen17@gmail.com>
2019-04-08 12:38:35 -0700

    Fix

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,55 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var res = '';
+  
+  if (typeof obj === 'undefined' || typeof obj === 'function') {
+    return undefined; 
+  } 
+
+  if (typeof obj === 'string') {
+    return '\"' + obj + '\"'; 
+  } 
+
+  if (obj === null) {
+    return 'null';
+  } 
+  if (typeof obj !== 'object') {
+
+    return '' + obj;  
+
+  } 
+
+  if (Array.isArray(obj)) {
+
+    var array = [];
+
+    for (var i = 0; i < obj.length; i++) {
+      array.push(stringifyJSON(obj[i]));
+    }
+
+    return '[' + array.join(',') + ']';
+    
+    
+  }
+
+  if (typeof obj === 'object') {
+    
+
+    var arr = [];
+    for (var key in obj) {
+     
+      var value = obj[key];
+      if (value !== 'undefined' && key !== 'undefined' && typeof value !== 'function') {
+        arr.push(stringifyJSON(key) + ':' + stringifyJSON(value));
+      }
+     
+    }
+    return '{' + arr.join(',') + '}';
+   
+  }
+  return obj.toString();
+
+  
+
 };
